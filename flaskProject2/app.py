c89afc99db24e09fc5ed076d1285fa646274c4db
fugde08 <119936523+fugde08@users.noreply.github.com>
2024-04-09 13:12:04 +0100

    Add files via upload

---
+++ flaskProject2/app.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from helper import pets
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return """ 
+  <h1>Adopt a Pet!</h1>
+  <p>Browse through the links below to find your new furry friend:</p>
+  <ul>
+  <li><a href = '/animals/dogs'>Dogs</li></a>
+  <li><a href = '/animals/cats'>Cats</li></a>
+  <li><a href = '/animals/rabbits'>Rabbits</li></a>
+  </ul>
+  """
+
+
+@app.route('/animals/<pet_type>')
+def animals(pet_type):
+    html = f'<h1>List of {pet_type}</h1>'
+    html += '<ul>'
+    for pet_id, x in enumerate(pets[pet_type]):
+        name = x["name"]
+        html += f'<li><a href="/animals/{pet_type}/{pet_id}"> {name} </a></li>'
+
+    html += '</ul>'
+    return html
+
+
+@app.route('/animals/<pet_type>/<int:pet_id>')
+def pet(pet_type, pet_id):
+    pet = pets[pet_type][pet_id]
+    return f'''
+  <h1> {pet["name"]} </h1>
+  <img src="{pet["url"]}"/>
+  <p>{pet["description"]}</p>
+  <ul>
+    <li>breed: {pet["breed"]}</li>
+    <li>age: {pet["age"]}</li>
+  </ul>
+  '''
